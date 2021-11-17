cff40557d9b10937d01ec28de45a2691e8d8fa3a
christian-dinh <christian.t.dinh@gmail.com>
2021-11-17 17:40:04 -0500

    init repo with update script and github action

---
+++ update-cotw.ts
@@ -0,0 +1,28 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import glob from 'glob';
+import path from 'path';
+
+const storePath = path.join(__dirname, 'docs/bin/concept-of-the-week.txt');
+
+const getRandomElement = (arr: any[]) =>
+  arr[Math.floor(Math.random() * arr.length)];
+
+execSync('git clone git@github.com:Codecademy/docs.git', {
+  stdio: [0, 1, 2],
+});
+
+const conceptPaths = glob.sync('docs/content/*/concepts/*/*.md');
+
+const currentConceptPath = fs.existsSync(storePath)
+  ? fs.readFileSync(storePath)
+  : '';
+
+let newConceptPath: string;
+do {
+  newConceptPath = getRandomElement(conceptPaths);
+} while (newConceptPath === currentConceptPath);
+
+fs.writeFileSync(storePath, newConceptPath, 'utf8');
+
+console.log(`Update successful: ${currentConceptPath} -> ${newConceptPath}`);
