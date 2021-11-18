c4708966dfc64c017794188414b447694308ae65
christian-dinh <christian.t.dinh@gmail.com>
2021-11-18 12:20:39 -0500

    log pwd

--- update-cotw.ts
+++ update-cotw.ts
@@ -8,6 +8,10 @@ const storePath = path.join(__dirname, 'docs/bin/concept-of-the-week.txt');
 const getRandomElement = (arr: any[]) =>
   arr[Math.floor(Math.random() * arr.length)];
 
+execSync('pwd', {
+  stdio: [0, 1, 2],
+});
+
 execSync('git clone git@github.com:Codecademy/docs.git', {
   stdio: [0, 1, 2],
 });
